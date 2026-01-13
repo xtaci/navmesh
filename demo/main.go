0a3cd803d615c64f1ceb8f1e7a581b33e186298b
xtaci <imap@live.com>
2026-01-13 22:47:34 +0800

    use fyne

--- demo/main.go
+++ demo/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/google/gxui"
-	"github.com/google/gxui/drivers/gl"
-	"github.com/google/gxui/math"
-	"github.com/google/gxui/samples/flags"
-	. "github.com/spate/vectormath"
-	. "github.com/xtaci/navmesh"
+	"image/color"
 	"log"
 	"os"
-	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	. "github.com/spate/vectormath"
+	. "github.com/xtaci/navmesh"
+)
+
+const (
+	windowWidth  = 800
+	windowHeight = 600
+	SCALE_FACTOR = float32(0.15)
 )
 
 type List struct {
@@ -18,194 +26,255 @@ type List struct {
 	Triangles [][3]int32
 }
 
-var list List
-var vertices []Point3
-var triangles [][3]int32
-var dijkstra Dijkstra
-
 func main() {
-	f, err := os.Open("mesh.json")
+	meshFile, err := os.Open("mesh.json")
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := json.NewDecoder(f).Decode(&list); err != nil {
+	defer meshFile.Close()
+
+	var list List
+	if err := json.NewDecoder(meshFile).Decode(&list); err != nil {
 		log.Fatal(err)
 	}
-	vertices = list.Vertices
-	triangles = list.Triangles
-	dijkstra.CreateMatrixFromMesh(Mesh{vertices, triangles})
-	gl.StartDriver(appMain)
+
+	dijkstra := &Dijkstra{}
+	dijkstra.CreateMatrixFromMesh(Mesh{Vertices: list.Vertices, Triangles: list.Triangles})
+
+	application := app.New()
+	window := application.NewWindow("navmesh")
+	window.Resize(fyne.NewSize(windowWidth, windowHeight))
+
+	navWidget := newNavMeshWidget(list.Vertices, list.Triangles, dijkstra)
+	window.SetContent(navWidget)
+	window.ShowAndRun()
 }
 
-var SCALE_FACTOR = float32(0.15)
-
-func appMain(driver gxui.Driver) {
-	theme := flags.CreateTheme(driver)
-	window := theme.CreateWindow(800, 600, "navmesh")
-	canvas := driver.CreateCanvas(math.Size{W: 800, H: 600})
-
-	// mouse
-	isStart := true
-	var src_id, dest_id int32 // source & dest triangle id
-	var src, dest Point3
-	window.OnMouseDown(func(me gxui.MouseEvent) {
-		pt := Point3{X: float32(me.Point.X) / SCALE_FACTOR, Y: float32(me.Point.Y) / SCALE_FACTOR}
-		id := getTriangleId(pt)
-		if isStart {
-			src_id = id
-			src = pt
-		} else {
-			dest_id = id
-			dest = pt
-		}
-		if !isStart {
-			if src_id != -1 && dest_id != -1 {
-				canvas := route(driver, src_id, dest_id, src, dest)
-				image := theme.CreateImage()
-				image.SetCanvas(canvas)
-				window.AddChild(image)
-			}
-		}
-		isStart = !isStart
-	})
-
-	// draw mesh
-	for k := 0; k < len(triangles); k++ {
-		poly := []gxui.PolygonVertex{
-			gxui.PolygonVertex{
-				Position: math.Point{
-					int(SCALE_FACTOR * vertices[triangles[k][0]].X),
-					int(SCALE_FACTOR * vertices[triangles[k][0]].Y),
-				}},
-
-			gxui.PolygonVertex{
-				Position: math.Point{
-					int(SCALE_FACTOR * vertices[triangles[k][1]].X),
-					int(SCALE_FACTOR * vertices[triangles[k][1]].Y),
-				}},
-
-			gxui.PolygonVertex{
-				Position: math.Point{
-					int(SCALE_FACTOR * vertices[triangles[k][2]].X),
-					int(SCALE_FACTOR * vertices[triangles[k][2]].Y),
-				}},
-		}
-		canvas.DrawPolygon(poly, gxui.CreatePen(3, gxui.Gray80), gxui.CreateBrush(gxui.Gray40))
-		//canvas.DrawPolygon(poly, gxui.CreatePen(2, gxui.Red), gxui.CreateBrush(gxui.Yellow))
-	}
-
-	canvas.Complete()
-	image := theme.CreateImage()
-	image.SetCanvas(canvas)
-	window.AddChild(image)
-	window.OnClose(driver.Terminate)
+type navMeshWidget struct {
+	widget.BaseWidget
+	container      *fyne.Container
+	background     *canvas.Rectangle
+	vertices       []Point3
+	triangles      [][3]int32
+	dijkstra       *Dijkstra
+	selectingStart bool
+	startPoint     Point3
+	destPoint      Point3
+	startTriangle  int32
+	destTriangle   int32
+	markerObjects  []fyne.CanvasObject
+	pathObjects    []fyne.CanvasObject
 }
 
-func route(driver gxui.Driver, src_id, dest_id int32, src, dest Point3) (canvas gxui.Canvas) {
-	defer func() {
-		canvas.Complete()
-	}()
-	canvas = driver.CreateCanvas(math.Size{W: 800, H: 600})
-
-	t0 := time.Now()
-	for i := 0; i < 1000; i++ {
-		// Phase 1. Use Dijkstra to find shortest path on Triangles
-		path := dijkstra.Run(src_id)
-
-		// Phase 2.  construct path indices
-		// Check if this path include src & dest
-		path_triangle := [][3]int32{triangles[dest_id]}
-		prev_id := dest_id
-		for {
-			cur_id := path[prev_id]
-			if cur_id == -1 {
-				return canvas
-			}
-			path_triangle = append([][3]int32{triangles[cur_id]}, path_triangle...)
-			if cur_id == src_id {
-				break
-			}
-			prev_id = cur_id
-		}
-
-		// Phase 3. use Navmesh to construct line
-		start, end := &Point3{X: src.X, Y: src.Y}, &Point3{X: dest.X, Y: dest.Y}
-		nm := NavMesh{}
-		trilist := TriangleList{vertices, path_triangle}
-		nm.Route(trilist, start, end)
+func newNavMeshWidget(vertices []Point3, triangles [][3]int32, dijkstra *Dijkstra) *navMeshWidget {
+	bg := canvas.NewRectangle(color.NRGBA{R: 18, G: 18, B: 26, A: 255})
+	content := container.NewWithoutLayout(bg)
+	w := &navMeshWidget{
+		container:      content,
+		background:     bg,
+		vertices:       vertices,
+		triangles:      triangles,
+		dijkstra:       dijkstra,
+		selectingStart: true,
+		startTriangle:  -1,
+		destTriangle:   -1,
 	}
-	log.Println("navmesh time:", time.Now().Sub(t0))
-	// Phase 1. Use Dijkstra to find shortest path on Triangles
-	path := dijkstra.Run(src_id)
+	w.ExtendBaseWidget(w)
+	w.drawMesh()
+	return w
+}
 
-	// Phase 2.  construct path indices
-	// Check if this path include src & dest
-	path_triangle := [][3]int32{triangles[dest_id]}
-	prev_id := dest_id
+type navMeshRenderer struct {
+	widget  *navMeshWidget
+	objects []fyne.CanvasObject
+}
+
+func (n *navMeshWidget) CreateRenderer() fyne.WidgetRenderer {
+	return &navMeshRenderer{
+		widget:  n,
+		objects: []fyne.CanvasObject{n.container},
+	}
+}
+
+func (r *navMeshRenderer) Destroy() {}
+
+func (r *navMeshRenderer) Layout(size fyne.Size) {
+	r.widget.container.Resize(size)
+	r.widget.background.Resize(size)
+}
+
+func (r *navMeshRenderer) MinSize() fyne.Size {
+	return fyne.NewSize(windowWidth, windowHeight)
+}
+
+func (r *navMeshRenderer) Objects() []fyne.CanvasObject {
+	return r.objects
+}
+
+func (r *navMeshRenderer) Refresh() {
+	canvas.Refresh(r.widget.container)
+}
+
+func (r *navMeshRenderer) BackgroundColor() color.Color {
+	return color.Transparent
+}
+
+func (n *navMeshWidget) drawMesh() {
+	edgeColor := color.NRGBA{R: 120, G: 130, B: 150, A: 255}
+	for _, tri := range n.triangles {
+		pts := []fyne.Position{
+			fyne.NewPos(SCALE_FACTOR*n.vertices[tri[0]].X, SCALE_FACTOR*n.vertices[tri[0]].Y),
+			fyne.NewPos(SCALE_FACTOR*n.vertices[tri[1]].X, SCALE_FACTOR*n.vertices[tri[1]].Y),
+			fyne.NewPos(SCALE_FACTOR*n.vertices[tri[2]].X, SCALE_FACTOR*n.vertices[tri[2]].Y),
+		}
+		for i := 0; i < 3; i++ {
+			next := (i + 1) % 3
+			line := canvas.NewLine(edgeColor)
+			line.StrokeWidth = 1
+			line.Position1 = pts[i]
+			line.Position2 = pts[next]
+			n.container.Add(line)
+		}
+	}
+}
+
+func (n *navMeshWidget) Tapped(ev *fyne.PointEvent) {
+	world := Point3{X: float32(ev.Position.X) / SCALE_FACTOR, Y: float32(ev.Position.Y) / SCALE_FACTOR}
+	tri := n.triangleAt(world)
+	if tri == -1 {
+		return
+	}
+
+	if n.selectingStart {
+		n.startPoint = world
+		n.startTriangle = tri
+		n.destTriangle = -1
+		n.selectingStart = false
+		n.clearObjects(&n.pathObjects)
+		n.updateMarkers()
+		return
+	}
+
+	n.destPoint = world
+	n.destTriangle = tri
+	n.selectingStart = true
+	n.updateMarkers()
+	n.drawPath()
+}
+
+func (n *navMeshWidget) triangleAt(pt Point3) int32 {
+	for i, tri := range n.triangles {
+		if insideTriangle(pt,
+			Point3{X: n.vertices[tri[0]].X, Y: n.vertices[tri[0]].Y},
+			Point3{X: n.vertices[tri[1]].X, Y: n.vertices[tri[1]].Y},
+			Point3{X: n.vertices[tri[2]].X, Y: n.vertices[tri[2]].Y}) {
+			return int32(i)
+		}
+	}
+	return -1
+}
+
+func (n *navMeshWidget) updateMarkers() {
+	n.clearObjects(&n.markerObjects)
+	if n.startTriangle != -1 {
+		n.markerObjects = append(n.markerObjects, n.addMarker(n.startPoint, color.NRGBA{R: 220, G: 200, B: 60, A: 255}))
+	}
+	if n.destTriangle != -1 {
+		n.markerObjects = append(n.markerObjects, n.addMarker(n.destPoint, color.NRGBA{R: 90, G: 180, B: 255, A: 255}))
+	}
+	n.Refresh()
+}
+
+func (n *navMeshWidget) addMarker(pt Point3, col color.Color) fyne.CanvasObject {
+	circle := canvas.NewCircle(col)
+	size := float32(10)
+	circle.Resize(fyne.NewSize(size, size))
+	circle.Move(fyne.NewPos(SCALE_FACTOR*pt.X-size/2, SCALE_FACTOR*pt.Y-size/2))
+	n.container.Add(circle)
+	return circle
+}
+
+func (n *navMeshWidget) drawPath() {
+	n.clearObjects(&n.pathObjects)
+	if n.startTriangle == -1 || n.destTriangle == -1 {
+		n.Refresh()
+		return
+	}
+
+	points := n.computeRoutePoints(n.startTriangle, n.destTriangle, n.startPoint, n.destPoint)
+	if len(points) < 2 {
+		n.Refresh()
+		return
+	}
+
+	pathColor := color.NRGBA{R: 60, G: 210, B: 130, A: 255}
+	for i := 0; i < len(points)-1; i++ {
+		line := canvas.NewLine(pathColor)
+		line.StrokeWidth = 3
+		line.Position1 = fyne.NewPos(SCALE_FACTOR*points[i].X, SCALE_FACTOR*points[i].Y)
+		line.Position2 = fyne.NewPos(SCALE_FACTOR*points[i+1].X, SCALE_FACTOR*points[i+1].Y)
+		n.container.Add(line)
+		n.pathObjects = append(n.pathObjects, line)
+	}
+	n.Refresh()
+}
+
+func (n *navMeshWidget) computeRoutePoints(srcID, destID int32, src, dest Point3) []Point3 {
+	if srcID < 0 || destID < 0 || int(srcID) >= len(n.triangles) || int(destID) >= len(n.triangles) {
+		return nil
+	}
+
+	path := n.dijkstra.Run(srcID)
+	if int(destID) >= len(path) {
+		return nil
+	}
+
+	pathTriangles := [][3]int32{n.triangles[destID]}
+	prev := destID
 	for {
-		cur_id := path[prev_id]
-		if cur_id == -1 {
-			return canvas
+		cur := path[prev]
+		if cur == -1 {
+			return nil
 		}
-		path_triangle = append([][3]int32{triangles[cur_id]}, path_triangle...)
-		if cur_id == src_id {
+		pathTriangles = append([][3]int32{n.triangles[cur]}, pathTriangles...)
+		if cur == srcID {
 			break
 		}
-		prev_id = cur_id
+		prev = cur
+		if prev < 0 || int(prev) >= len(path) {
+			return nil
+		}
 	}
 
-	// Phase 3. use Navmesh to construct line
-	start, end := &Point3{X: src.X, Y: src.Y}, &Point3{X: dest.X, Y: dest.Y}
+	startCopy := src
+	endCopy := dest
 	nm := NavMesh{}
-	trilist := TriangleList{vertices, path_triangle}
-	r, _ := nm.Route(trilist, start, end)
-
-	var poly []gxui.PolygonVertex
-	poly = append(poly,
-		gxui.PolygonVertex{
-			Position: math.Point{
-				int(SCALE_FACTOR * start.X),
-				int(SCALE_FACTOR * start.Y),
-			}})
-
-	for k := range r.Line {
-		poly = append(poly,
-			gxui.PolygonVertex{
-				Position: math.Point{
-					int(SCALE_FACTOR * r.Line[k].X),
-					int(SCALE_FACTOR * r.Line[k].Y),
-				}})
+	r, err := nm.Route(TriangleList{Vertices: n.vertices, Triangles: pathTriangles}, &startCopy, &endCopy)
+	if err != nil {
+		return nil
 	}
-	poly = append(poly,
-		gxui.PolygonVertex{
-			Position: math.Point{
-				int(SCALE_FACTOR * end.X),
-				int(SCALE_FACTOR * end.Y),
-			}})
 
-	canvas.DrawLines(poly, gxui.CreatePen(2, gxui.Green))
-	return
+	points := make([]Point3, 0, len(r.Line)+2)
+	points = append(points, src)
+	points = append(points, r.Line...)
+	points = append(points, dest)
+	return points
+}
+
+func (n *navMeshWidget) clearObjects(objects *[]fyne.CanvasObject) {
+	for _, obj := range *objects {
+		n.container.Remove(obj)
+	}
+	*objects = nil
+}
+
+func insideTriangle(pt, v1, v2, v3 Point3) bool {
+	b1 := sign(pt, v1, v2) <= 0
+	b2 := sign(pt, v2, v3) <= 0
+	b3 := sign(pt, v3, v1) <= 0
+	return (b1 == b2) && (b2 == b3)
 }
 
 func sign(p1, p2, p3 Point3) float32 {
 	return (p1.X-p3.X)*(p2.Y-p3.Y) - (p2.X-p3.X)*(p1.Y-p3.Y)
-}
-
-func inside(pt, v1, v2, v3 Point3) bool {
-	b1 := sign(pt, v1, v2) <= 0
-	b2 := sign(pt, v2, v3) <= 0
-	b3 := sign(pt, v3, v1) <= 0
-	return ((b1 == b2) && (b2 == b3))
-}
-
-func getTriangleId(pt Point3) (id int32) {
-	for k := 0; k < len(triangles); k++ {
-		if inside(pt,
-			Point3{X: vertices[triangles[k][0]].X, Y: vertices[triangles[k][0]].Y},
-			Point3{X: vertices[triangles[k][1]].X, Y: vertices[triangles[k][1]].Y},
-			Point3{X: vertices[triangles[k][2]].X, Y: vertices[triangles[k][2]].Y}) {
-			return int32(k)
-		}
-	}
-	return -1
 }
